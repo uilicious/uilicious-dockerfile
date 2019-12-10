df389ebaa8ffe72ab04166db0224a45accdb49de
Eugene Cheah <eugeneqin@gmail.com>
2019-12-10 14:09:36 +0800

    Tweaks to how nginx-base dns config is setup

--- network/nginx-base/configure-dns-resolver.sh
+++ network/nginx-base/configure-dns-resolver.sh
@@ -1,14 +1,21 @@
 #!/bin/sh
-# Fetch the DNS resolver
-RESOLVER="$DNS"
-if [ -z "$RESOLVER" ]; then
-    RESOLVER=$(cat /etc/resolv.conf | grep "nameserver" | awk "{print \$2}")
-fi
 
-if [ -n "$DNS_VALID_TIMEOUT" ]; then
-    RESOLVER="$RESOLVER valid=$DNS_VALID_TIMEOUT"
+# Lets setup `/etc/nginx/dns-resolver.conf` (if needed)
+if [ "$DNS_SETUP_CONF" = "1" ] 
+then
+    # Fetch the DNS resolver
+    RESOLVER="$DNS"
+    if [ -z "$RESOLVER" ]; then
+        RESOLVER=$(cat /etc/resolv.conf | grep "nameserver" | awk "{print \$2}")
+    fi
+
+    if [ -n "$DNS_VALID_TIMEOUT" ]; then
+        RESOLVER="$RESOLVER valid=$DNS_VALID_TIMEOUT"
+    fi
+    echo "resolver $RESOLVER ;" > /etc/nginx/dns-resolver.conf
+else
+    echo "" > /etc/nginx/dns-resolver.conf
 fi
-echo "resolver $RESOLVER ;" > /etc/nginx/dns-resolver.conf
 
 # Chain the execution commands
 exec "$@"
