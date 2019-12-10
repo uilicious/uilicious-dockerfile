6862f9c3e07354a9bd996dadcfffa9c8dcf99f2a
Eugene Cheah <eugeneqin@gmail.com>
2019-12-10 15:17:06 +0800

    config postscript

---
+++ misc/gcdns-cli/README.md
@@ -0,0 +1,5 @@
+# Google Cloud DNS Command Line Tool
+
+Prebuilt container of gcdns-cli : Used to perform Google Cloud DNS specific commands
+
+See : https://github.com/tilfin/gcdns-cli
--- uilicious/internal-nginx-base/config-copy.sh
+++ uilicious/internal-nginx-base/config-copy.sh
@@ -3,7 +3,7 @@
 # ENTRYPOINT_PRESCRIPT environment variable support
 
 if [ -n "$CONFIG_POSTSCRIPT" ]; then 
-	echo "### postscript of userpanel"
+	echo "### CONFIG_POSTSCRIPT"
 	eval "$CONFIG_POSTSCRIPT";
 	  # Chain the execution commands
 fi 
